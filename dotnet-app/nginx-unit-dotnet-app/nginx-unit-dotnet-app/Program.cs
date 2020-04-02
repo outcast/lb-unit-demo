8776b26c31fa80f437dde9d8871c8a2574ed2654
James Jones <jam.jones@nginx.com>
2020-04-02 21:40:09 +0000

    starting to work on dotnet

---
+++ dotnet-app/nginx-unit-dotnet-app/nginx-unit-dotnet-app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace nginx_unit_dotnet_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Content-Type: text/plain");
+            Console.WriteLine();
+            Console.WriteLine("Envs: ");
+            foreach (DictionaryEntry env in Environment.GetEnvironmentVariables())
+                Console.WriteLine("  {0} = {1}", env.Key, env.Value);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
